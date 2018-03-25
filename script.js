af6abf7415bc291effd63ffe0d3111652bb7f24c
Celso Neto <celsopalmeiraneto@gmail.com>
2018-03-24 23:31:56 -0300

    Adding support to multiple translation keys

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var translationKey, translationsBox, translationResult;
+var translationKeys, translationsBox, translationResult;
+var searchCounter = 0;
 var settings = {
   replicateSameLanguage: true
 }
@@ -13,11 +14,24 @@ const availableLanguages = [
 
 document.addEventListener('DOMContentLoaded', () =>{
   populateGlobalVars();
-  populateForm();
 }, false);
 
+
+
+function translationKeyKeyUp(){
+  searchCounter++;
+  setTimeout(populateFormDispatcher, 500);
+}
+
+function populateFormDispatcher(){
+  searchCounter--;
+  if(searchCounter === 0) return populateForm();
+}
+
 function populateGlobalVars(){
-  translationKey = document.getElementById("translationKey");
+  translationKeys = document.getElementById("translationKeys");
+  translationKeys.onkeyup = translationKeyKeyUp;
+
   translationsBox = document.getElementById("translationsBox");
   translationResult = document.getElementById("translationResult");
   translationResult.onfocus = copyToClipboardOnFocus;
@@ -35,39 +49,63 @@ function copyToClipboardOnFocus(e){
 }
 
 function populateForm(){
-  var template = `<label>Tradução</label>
+  const itemTemplate = `<label>Tradução</label>
             <input type="text" class="form-control form-control-sm translationText" value="">`;
-  var addedLanguages = [];
-  availableLanguages.map((v) => {
-    let item = document.createElement("div");
-    item.innerHTML = template;
-    item.classList.add("form-group");
 
-    item.children.item(0).textContent = v.text;
-    item.children.item(1).dataset.id = v.id;
+  translationsBox.innerHTML = ""; // TODO: Make a delta of changes and apply just where it's necessary.
 
-    if(addedLanguages.includes(v.id.substr(0, 2)) && settings.replicateSameLanguage){
-      item.classList.add("d-none");
-    }else{
-      addedLanguages.push(v.id.substr(0, 2));
-    }
+  var keys = parseKeys();
 
+  keys.forEach(key => {
+    var addedLanguages = [];
 
-    item.onkeyup = (e) => {
-      if(settings.replicateSameLanguage){
-          replicateText(e);
+    let headerKey = document.createElement("h5");
+    headerKey.textContent = key;
+
+    let group = document.createElement("div");
+    group.classList.add("list-group");
+    group.classList.add("list-group-flush");
+
+    var groupItem = document.createElement("div");
+    groupItem.classList.add("list-group-item");
+    groupItem.append(headerKey);
+
+    group.appendChild(groupItem);
+
+    translationsBox.appendChild(group);
+
+    availableLanguages.forEach(v => {
+      let item = document.createElement("div");
+      item.innerHTML = itemTemplate;
+      item.classList.add("form-group");
+
+      item.children.item(0).textContent = v.text;
+      item.children.item(1).dataset.key = key;
+      item.children.item(1).dataset.id = v.id;
+
+      if(addedLanguages.includes(v.id.substr(0, 2)) && settings.replicateSameLanguage){
+        item.classList.add("d-none");
+      }else{
+        addedLanguages.push(v.id.substr(0, 2));
       }
-      generateResults();
-    }
 
-    translationsBox.appendChild(item);
+      item.onkeyup = (e) => {
+        if(settings.replicateSameLanguage){
+            replicateText(e);
+        }
+        generateResults();
+      }
+
+      groupItem.appendChild(item);
+    });
   });
 }
 
 function replicateText(e){
   var language = e.target.dataset.id.substr(0, 2);
+  var key = e.target.dataset.key;
   getTranslationFields().forEach(v => {
-    if(e.target !== v && v.dataset.id.startsWith(language)){
+    if(e.target !== v && key === v.dataset.key && v.dataset.id.startsWith(language)){
       v.value = e.target.value;
     }
   });
@@ -77,12 +115,21 @@ function getTranslationFields(){
   return Array.from(document.getElementsByClassName("translationText"));
 }
 
+function parseKeys(){
+  return translationKeys.value.split("\n").reduce((acc, v) => {
+    if(typeof v === "string" && v.trim() !== ""){
+      acc.push(v.trim());
+    }
+    return acc;
+  }, []);
+}
+
 function generateResults(){
   let translations = getTranslationFields();
 
   let result = "BEGIN\n" + translations.reduce((acc, v) => {
     if(v && v.value && v.value.trim() !== ""){
-      acc += `    PR_ATUALIZA_INT_MESSAGE('${v.dataset.id}', '${translationKey.value}', '${v.value}', 'S');\n`;
+      acc += `    PR_ATUALIZA_INT_MESSAGE('${v.dataset.id}', '${v.dataset.key}', '${v.value}', 'S');\n`;
       return acc;
     }
     return acc;
